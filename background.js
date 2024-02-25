f4931b1d961f9a09af6242d26f97dbd65a326238
Jessie Newman <Jessie-Newman@users.noreply.github.com>
2024-02-24 21:28:03 -0500

    popup is now one button
    No longer requires the markedown, just click "place bets" when active tab is the correct foodclub page

--- background.js
+++ background.js
@@ -1,84 +1,56 @@
-dict = {
-    'Dan': '1', 
-    'Sproggie': '2',
-    'Orvin': '3',
-    'Lucky': '4',
-    'Edmund': '5',
-    'Peg Leg': '6',
-    'Bonnie': '7',
-    'Puffo': '8',
-    'Stuff': '9',
-    'Squire': '10',
-    'Crossblades': '11',
-    'Stripey':  '12',
-    'Ned': '13',
-    'Fairfax': '14',
-    'Gooblah':  '15',
-    'Franchisco':  '16',
-    'Federismo': '17',
-    'Blackbeard':  '18',
-    'Buck': '19',
-    'Tailhook':  '20'
-}
+function parseTable() {
+    competitors_to_id = {
+        'Dan': '1', 
+        'Sproggie': '2',
+        'Orvin': '3',
+        'Lucky': '4',
+        'Edmund': '5',
+        'Peg Leg': '6',
+        'Bonnie': '7',
+        'Puffo': '8',
+        'Stuff': '9',
+        'Squire': '10',
+        'Crossblades': '11',
+        'Stripey':  '12',
+        'Ned': '13',
+        'Fairfax': '14',
+        'Gooblah':  '15',
+        'Franchisco':  '16',
+        'Federismo': '17',
+        'Blackbeard':  '18',
+        'Buck': '19',
+        'Tailhook':  '20'
+    }
 
+    let all_competitors = []
+    table = document.getElementsByClassName('chakra-table')[0].children[1]
+    for (let i=0; i<10; i++) {
+        competitors = []
+        row = table.children[i]
+        for (let j=8; j<13; j++) {
+            cell = row.children[j]
+            competitors.push(competitors_to_id[cell.textContent])
+        }
+        all_competitors.push(competitors)
+    }
+    return all_competitors
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.command === "parse") {
-        parseMarkdownTable(request.table)
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({
+              target: {tabId: tabs[0].id},
+              function: parseTable
+            }, function(results) {
+              openTabs(results[0]['result'])
+            });
+          });
         sendResponse({status: "success", message: "noice"});
     }
 });
 
-
-function populateCompetitors(competitors) {
-    competitors.map(number => {
-        shift = 11
-        bolds = document.getElementsByTagName('b')
-        next = false
-        maxBet = null
-        Array.from(bolds).forEach((tag) => {
-            if (next && !maxBet) { 
-                maxBet = tag.innerHTML;
-            } else if (tag.innerHTML == 'Leave') {
-                next = true
-            }
-        })
-        document.getElementsByTagName('input')[shift+5].value = maxBet
-
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < document.getElementsByTagName('select')[0].options.length; j++) {
-                if (document.getElementsByTagName('select')[i].options[j].value == number) {
-                    document.getElementsByTagName('input')[shift+i].click()
-                    document.getElementsByTagName('select')[i].value = number
-                }
-            }
-        }
-    });
-}
-
-function parseMarkdownTable(markdownTable) {
-    const rows = markdownTable.trim().split('\n');
-
-    let validColumns = [];
-    rows.map(row => {
-        const columns = row.split('|').map(column => column.trim())
-        if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'].includes(columns[0])) {
-            validColumns.push(columns)
-        }
-    })
-
-    let all_competitors = []
-    validColumns.map(row => {
-        let competitors = []
-        row.map(cell => {
-            if (cell in dict) {
-                competitors.push(dict[cell])
-            }
-        })
-        all_competitors.push(competitors)
-    })
-
-
+function openTabs(all_competitors) {
     all_competitors.map(competitors => {
         chrome.tabs.create({url: "https://www.neopets.com/pirates/foodclub.phtml?type=bet"}, function(tab) {
             chrome.tabs.onUpdated.addListener(function listener (tabId, info) {
@@ -86,7 +58,7 @@ function parseMarkdownTable(markdownTable) {
                     chrome.tabs.onUpdated.removeListener(listener);
                     chrome.scripting.executeScript({
                         target: {tabId: tab.id},
-                        func: populateCompetitors,
+                        func: populateBets,
                         args: [competitors]
                     }).then(result => {
                         console.log('Script result:', result);
@@ -99,3 +71,25 @@ function parseMarkdownTable(markdownTable) {
     });
 }
 
+function populateBets(competitors) {
+    competitors.map(number => {
+        shift = 11
+        bolds = document.getElementsByTagName('b')
+        let i = 0
+        Array.from(bolds).some((tag) => {
+            i++
+            return tag.innerHTML == 'Leave'
+        })
+        maxBet = bolds[i].innerHTML
+        document.getElementsByTagName('input')[shift+5].value = maxBet
+
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < document.getElementsByTagName('select')[0].options.length; j++) {
+                if (document.getElementsByTagName('select')[i].options[j].value == number) {
+                    document.getElementsByTagName('input')[shift+i].checked = true
+                    document.getElementsByTagName('select')[i].value = number
+                }
+            }
+        }
+    });
+}
--- content.js
+++ content.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var button = document.querySelector('button');
   button.addEventListener('click', function(event) {
     event.preventDefault();
-    var table = document.getElementById('textarea').value;
     chrome.runtime.sendMessage({
       command: "parse",
-      table: table,
     }, function(response) {
       console.log(response);
     });
